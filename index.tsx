e739fa7578a73e812a999257774cdf0511d73257
Richard Luvis <lopezcard35@gmail.com>
2025-11-28 13:36:08 -0500

    cambios en vite

--- index.tsx
+++ index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from "react-router-dom";
 import App from './App';
 
 const rootElement = document.getElementById('root');
@@ -12,6 +13,8 @@ const root = ReactDOM.createRoot(rootElement);
 
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter basename="/Portafolio">
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
